5f0dc56100b787e2fe2abdc0037c090f88c5c690
Krantiditya .(krantiditya) <krantiditya@infosys.com>
2025-04-14 19:23:21 +0530

    add s3 objects

--- s3_operations.py
+++ s3_operations.py
@@ -7,6 +7,17 @@ BUCKET_NAME = "data-bucket-s797866"
 
 script_dir = dirname(__file__)
 
+tags = {
+  "tagA": "10",
+  "tagB": "20"
+}
+
+meta_data = {
+    "meta_key": "678"             
+}
+
+tags_str = "&".join(f"{key}={value}" for key, value in tags.items())
+
 def add_s3_objects():
 
     s3_client = boto3.client('s3', region_name="ap-south-1")
@@ -17,11 +28,12 @@ def add_s3_objects():
         with open(filename, "w") as file:
             file.write("text document " + str(i))
         file.close()
+        
         s3_client.upload_file(filepath, 
                               BUCKET_NAME, 
                               filename,  
-                              ExtraArgs={'Metadata': {'meta_key': '678'}                                         
-                                         })
+                              ExtraArgs={'Metadata': meta_data, 
+                                         'Tagging': tags_str})
         print(f"{filename} uploaded!")
         os.remove(filepath)
 
